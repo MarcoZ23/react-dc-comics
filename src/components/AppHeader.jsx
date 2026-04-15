1691fa0ecdee9ec76e3cdba33a132648fecac092
MarcoZ23 <zollomarco23@gmail.com>
2026-04-15 23:25:21 +0200

    add font bootstrap and complete header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,6 +1,25 @@
+import dcLogo from "../assets/img/dc-logo.png"
 export default function AppHeader() {
 
     return (
-        <header>Test</header>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <img src={dcLogo} alt='logo Dc' />
+            <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav ms-auto">
+                        <a className="nav-link" href="#">CHARACTERS</a>
+                        <a className="nav-link text-primary borde border-bottom border-primary" href="#">COMICS</a>
+                        <a className="nav-link" href="#">MOVIES</a>
+                        <a className="nav-link" href="#">TV</a>
+                        <a className="nav-link" href="#">GAMES</a>
+                        <a className="nav-link" href="#">COLLECTIBLES</a>
+                        <a className="nav-link" href="#">VIDEOS</a>
+                        <a className="nav-link" href="#">FANS</a>
+                        <a className="nav-link" href="#">NEWS</a>
+                        <a className="nav-link" href="#">SHOP</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
 }
