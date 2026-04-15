ab2f8ebcc7e28499b2c1bda03139134216a0c7d9
MarcoZ23 <zollomarco23@gmail.com>
2026-04-15 22:08:52 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import jumboJpg from "./assets/img/jumbotron.jpg"
+import youtubeLogo from "./assets/img/footer-youtube.png"
+import twitterLogo from "./assets/img/footer-twitter.png"
+import pinterestLogo from "./assets/img/footer-pinterest.png"
+import periscopeLogo from "./assets/img/footer-periscope.png"
+import facebookLogo from "./assets/img/footer-facebook.png"
+import bgLogo from "./assets/img/footer-bg.jpg"
+import faviconLogo from "./assets/img/favicon.ico"
+import dcLogo from "./assets/img/dc-logo.png"
+import dcBgLogo from "./assets/img/dc-logo-bg.png"
+import comicsSub from "./assets/img/buy-comics-subscriptions.png"
+import comicsShop from "./assets/img/buy-comics-shop-locator.png"
+import comicsMerch from "./assets/img/buy-comics-merchandise.png"
+import comicsDigital from "./assets/img/buy-comics-digital-comics.png"
+import AppHeader from "./components/AppHeader"
+import AppMain from "./components/AppMain"
+import AppFooter from "./components/AppFooter"
+function App() {
+
+
+  return (
+    <>
+      <AppHeader />
+      <AppMain />
+      <AppFooter />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AppMain.jsx
@@ -0,0 +1,6 @@
+export default function AppMain() {
+
+    return (
+        <main>Test</main>
+    )
+}
