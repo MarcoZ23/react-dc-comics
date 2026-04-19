c59db5f496743226149a4182a442749e177f6597
MarcoZ23 <zollomarco23@gmail.com>
2026-04-19 13:23:27 +0200

    add .map for navbar in the AppHeader and fix error

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,22 +1,70 @@
 import dcLogo from "../assets/img/dc-logo.png"
 export default function AppHeader() {
 
+    const navEl = [
+        {
+            id: 1,
+            text: 'CHARACTERS',
+            path: '#'
+        },
+        {
+            id: 2,
+            text: 'COMICS',
+            path: '#'
+        },
+        {
+            id: 3,
+            text: 'MOVIES',
+            path: '#'
+        },
+        {
+            id: 4,
+            text: 'TV',
+            path: '#'
+        },
+        {
+            id: 5,
+            text: 'GAMES',
+            path: '#'
+        },
+        {
+            id: 6,
+            text: 'COLLECTIBLES',
+            path: '#'
+        },
+        {
+            id: 7,
+            text: 'VIDEOS',
+            path: '#'
+        },
+        {
+            id: 8,
+            text: 'FANS',
+            path: '#'
+        },
+        {
+            id: 9,
+            text: 'NEWS',
+            path: '#'
+        },
+        {
+            id: 10,
+            text: 'SHOP',
+            path: '#'
+        }
+    ]
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary ms-5">
             <img src={dcLogo} alt='logo Dc' className="head-logo" />
             <div className="container-fluid">
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav ms-auto fw-bold py-4">
-                        <a className="nav-link" href="#">CHARACTERS</a>
-                        <a className="nav-link text-primary border-bottom border-primary" href="#">COMICS</a>
-                        <a className="nav-link" href="#">MOVIES</a>
-                        <a className="nav-link" href="#">TV</a>
-                        <a className="nav-link" href="#">GAMES</a>
-                        <a className="nav-link" href="#">COLLECTIBLES</a>
-                        <a className="nav-link" href="#">VIDEOS</a>
-                        <a className="nav-link" href="#">FANS</a>
-                        <a className="nav-link" href="#">NEWS</a>
-                        <a className="nav-link" href="#">SHOP</a>
+                        {
+                            navEl.map((item) => (
+                                <a key={item.id} href={item.path} className="nav-link">{item.text}</a>
+
+                            ))
+                        }
                     </div>
                 </div>
             </div>
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -11,8 +11,8 @@ export default function AppMain() {
             <div className="container-fluid bg-black">
                 <div className="row">
                     {comics.map((comic) => (
-                        <div className="col-sm-6 col-md-4 col-lg-2">
-                            <div className="card mt-4 bg-black text-white" key={comic.id}>
+                        <div className="col-sm-6 col-md-4 col-lg-2" key={comic.id}>
+                            <div className="card mt-4 bg-black text-white">
                                 <img src={comic.thumb} alt={comic.title} className="img-card card-img-top"></img>
                                 <div className="card-body">
                                     <h4 className="">{comic.series}</h4>
